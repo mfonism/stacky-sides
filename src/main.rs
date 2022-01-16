27bc64a86d100c7a4d039c5871a2e34426520bc5
Mfon Eti-mfon <mfonetimfon@gmail.com>
2022-01-17 00:48:17 +0100

    Create hello world ish route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::routing::get;
+use axum::{Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 8000));
+    Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> &'static str {
+    "Hey there!"
 }
