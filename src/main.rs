28fdfcb4a2c2e31beb597d35ccdc89d2494c2ff7
Mfon Eti-mfon <mfonetimfon@gmail.com>
2022-01-17 01:28:37 +0100

    Add templating with tera

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use std::net::SocketAddr;
 
+use axum::extract::Extension;
+use axum::http::StatusCode;
+use axum::response::Html;
 use axum::routing::get;
-use axum::{Router, Server};
+use axum::{AddExtensionLayer, Router, Server};
+use tera::{Context, Tera};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(index));
+    let templates = match Tera::new("templates/**/*.html.tera") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(AddExtensionLayer::new(templates));
 
     let address = SocketAddr::from(([127, 0, 0, 1], 8000));
     Server::bind(&address)
@@ -14,6 +28,19 @@ async fn main() {
         .unwrap();
 }
 
-async fn index() -> &'static str {
-    "Hey there!"
+async fn index(
+    Extension(ref templates): Extension<Tera>,
+) -> Result<Html<String>, (StatusCode, String)> {
+    let mut context = Context::new();
+    context.insert("site_name", "Stacky Sides");
+    let body = templates
+        .render("chat/index.html.tera", &context)
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                String::from("Template error"),
+            )
+        })?;
+
+    Ok(Html(body))
 }
