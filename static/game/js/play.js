9ac92c37934e6cc05d78f00a0fd0685c188a7d5b
Mfon Eti-mfon <mfonetimfon@gmail.com>
2022-01-22 01:28:23 +0100

    Refresh game board after every move
    Ensure disabled cells are refreshed.

--- static/game/js/play.js
+++ static/game/js/play.js
@@ -1,20 +1,19 @@
 class GameUI {
-  constructor(playerNum) {
+  constructor(playerNum, gameBoardData) {
     this.playerNum = playerNum;
+    this.gameBoardData = gameBoardData;
   }
 
-  refreshGameBoard(boardData) {
-    document
-      .querySelector(".game-card")
-      .replaceWith(this.createGameCard(boardData));
+  refreshGameBoard() {
+    document.querySelector(".game-card").replaceWith(this.createGameCard());
 
-    this.canPlayNext = this.checkTurn(boardData) == this.playerNum;
+    this.canPlayNext = this.checkTurn() == this.playerNum;
   }
 
-  checkTurn(boardData) {
+  checkTurn() {
     // which player's turn is it?
     let res = [0, 0, 0];
-    boardData.forEach((row) => {
+    this.gameBoardData.forEach((row) => {
       row.forEach((cellData) => {
         if (res[cellData] !== 0) {
           res[cellData] += 1;
@@ -25,7 +24,7 @@ class GameUI {
     return res[1] > res[2] ? 2 : 1;
   }
 
-  createGameCard(boardData) {
+  createGameCard() {
     let cardElt = document.createElement("div");
     cardElt.classList.add(
       "game-card",
@@ -36,33 +35,33 @@ class GameUI {
       "flex-wrap"
     );
 
-    boardData.forEach((row, i) => {
-      row.forEach((cellData, j) => {
-        cardElt.appendChild(this.createGameCell(i, j, cellData, boardData));
+    this.gameBoardData.forEach((row, i) => {
+      row.forEach((_cellData, j) => {
+        cardElt.appendChild(this.createGameCell(i, j));
       });
     });
 
     return cardElt;
   }
 
-  createGameCell(i, j, cellData, boardData) {
+  createGameCell(i, j) {
     let cellElt = document.createElement("div");
     cellElt.classList.add("cell");
     cellElt.setAttribute("data-row", i);
     cellElt.setAttribute("data-col", j);
 
-    if (cellData == 1) {
+    if (this.gameBoardData[i][j] == 1) {
       cellElt.classList.add("black");
-    } else if (cellData == 2) {
+    } else if (this.gameBoardData[i][j] == 2) {
       cellElt.classList.add("white");
     } else {
       // prevent from selecting cells that have no selected
       // neighbours on either side of them and aren't on the
       // left or right ends of the board
       if (
-        0 < j < boardData[0].length - 1 &&
-        boardData[i][j - 1] === 0 &&
-        boardData[i][j + 1] === 0
+        0 < j < this.gameBoardData[0].length - 1 &&
+        this.gameBoardData[i][j - 1] === 0 &&
+        this.gameBoardData[i][j + 1] === 0
       ) {
         cellElt.classList.add("disabled");
       }
@@ -90,15 +89,13 @@ class GameUI {
       }
       this.canPlayNext = false;
 
-      if (playerNum == 1) {
-        event.target.classList.add("black");
-      } else if (playerNum == 2) {
-        event.target.classList.add("white");
-      }
+      let row = event.target.dataset.row;
+      let col = event.target.dataset.col;
+      this.gameBoardData[row][col] = playerNum;
 
-      websocket.send(
-        `Selection ${event.target.dataset.row} ${event.target.dataset.col}`
-      );
+      this.refreshGameBoard(gameBoardData);
+
+      websocket.send(`Selection ${row} ${col}`);
     });
   }
 }
@@ -114,8 +111,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("playerNum").textContent
   );
 
-  let gameUI = new GameUI(playerNum);
-  gameUI.refreshGameBoard(gameBoardData);
+  let gameUI = new GameUI(playerNum, gameBoardData);
+  gameUI.refreshGameBoard();
 
   const websocket = new WebSocket(gamePlaySocketUrl);
   gameUI.attachClickListener(websocket);
